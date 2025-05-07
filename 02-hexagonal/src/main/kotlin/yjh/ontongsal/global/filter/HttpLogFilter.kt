8888e6761f3e5d073e9635abe7364d86554976fe
jeongjihun <wlgns2234@naver.com>
2025-05-07 23:59:56 +0900

    feat: http 정보 로깅 필터 구현

--- 02-hexagonal/src/main/kotlin/yjh/ontongsal/global/filter/HttpLogFilter.kt
+++ 02-hexagonal/src/main/kotlin/yjh/ontongsal/global/filter/HttpLogFilter.kt
@@ -11,7 +11,7 @@ import org.springframework.web.util.ContentCachingRequestWrapper
 import org.springframework.web.util.ContentCachingResponseWrapper
 
 @Component
-class LogFilter(
+class HttpLogFilter(
     private val objectMapper: ObjectMapper
 ) : OncePerRequestFilter() {
 
@@ -24,26 +24,45 @@ class LogFilter(
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-
+        // 요청 정보 캐싱
         val req = ContentCachingRequestWrapper(request)
         val res = ContentCachingResponseWrapper(response)
-
         filterChain.doFilter(req, res)
 
-        // req 로그 출력
-        val reqJson = String(req.contentAsByteArray)
-        val compactJson = try {
+        // param 정보 추출
+        val queryParams = request.method
+            .takeIf { it == "GET" }
+            ?.let { getQueryParamsInfo(request) }
+            ?: "N/A"
+
+        // req body 정보 추출
+        var reqJson = String(req.contentAsByteArray)
+            .takeIf { it.isNotBlank() }
+            ?: "N/A"
+        val compactReqJson = try {
             objectMapper.readTree(reqJson).toString()
         } catch (e: Exception) {
             reqJson // JSON 형식이 아니면 원본 그대로 출력
         }
-        log.info { "[LogFilter] req : $compactJson" }
 
-        // res 로그 출력
-        val resJson = String(res.contentAsByteArray)
-        log.info { "[LogFilter] res : $resJson" }
+        // req body 정보 추출
+        var resJson = String(res.contentAsByteArray)
+            .takeIf { it.isNotBlank() }
+            ?: "N/A"
+
+        // request & response info 로그 출력
+        log.info { "[LogFilter] ${request.method} ${request.requestURI}" }
+        log.info { "[LogFilter] request body : $compactReqJson, params: $queryParams" }
+        log.info { "[LogFilter] response body : $resJson" }
 
         res.copyBodyToResponse()
     }
 
+    private fun getQueryParamsInfo(request: HttpServletRequest): String {
+        return request.parameterMap
+            .map { (key, values) -> "$key=${values.joinToString(",")}" }
+            .joinToString(", ")
+            .takeIf { it.isNotEmpty() }
+            ?: "N/A"
+    }
 }
