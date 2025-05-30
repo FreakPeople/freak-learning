3de20b4ecdcefaa53130c1425abae07005c0dbbc
jeongjihun <wlgns2234@naver.com>
2025-05-31 00:36:14 +0900

    init: 멀티모듈 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,63 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
 }
 
 group = "yjh.ontongsal"
 version = "1.0-SNAPSHOT"
 
+repositories {
+    mavenCentral()
+}
+
 subprojects {
     apply(plugin = "java")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+    /**
+     * 자바, 코틀린 컴파일러가 JDK21 사용하도록 지정.
+     */
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
 
     repositories {
         mavenCentral()
     }
 
-    tasks.test {
+    dependencies {
+
+    }
+
+    /**
+     * 코틀린 컴파일러 추가옵션 설정.
+     * java 코드에서 @Nullable 또는 @NonNull 어노테이션이 명시된 부분에 대해 코틀린 null 처리방식을 엄격하게 적용한다.
+     */
+    kotlin {
+        compilerOptions {
+            freeCompilerArgs.addAll("-Xjsr305=strict")
+        }
+    }
+
+    tasks.withType<Test> {
         useJUnitPlatform()
     }
+}
+
+tasks.named<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,1 @@
 rootProject.name = "freak-learning"
-
